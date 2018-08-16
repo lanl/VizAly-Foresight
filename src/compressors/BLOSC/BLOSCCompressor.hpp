0a162dc48e6b211e308a667d7e548a47b1875eef
Pascal Grosset <pascal.av.grosset@gmail.com>
2018-08-16 16:50:48 -0600

    created interface for compressors

---
+++ src/compressors/BLOSC/BLOSCCompressor.hpp
@@ -0,0 +1,52 @@
+#ifndef _BLOSC_COMPRESSOR_H_
+#define _BLOSC_COMPRESSOR_H_
+
+#include <sstream>
+#include "compressorInterface.hpp"
+#include "timer.hpp"
+
+class BLOSCCompressor: public CompressorInterface
+{
+    
+  public:
+    BLOSCCompressor();
+    ~BLOSCCompressor();
+
+    void init();
+    int compress(void *input, void *output, size_t dataType, size_t n);
+    int decompress(void *input, void *output, size_t dataType, size_t n);
+    int close();
+};
+
+inline BLOSCCompressor::BLOSCCompressor()
+{
+    compressorName = "BLOSC";
+}
+
+inline BLOSCCompressor::~BLOSCCompressor()
+{
+    
+}
+
+
+inline void BLOSCCompressor::init()
+{
+
+}
+
+inline int BLOSCCompressor::compress(void *input, void *output, size_t dataType, size_t n)
+{
+
+}
+
+inline int BLOSCCompressor::decompress(void *input, void *output, size_t dataType, size_t n)
+{
+
+}
+
+inline int BLOSCCompressor::close()
+{
+
+}
+
+#endif
