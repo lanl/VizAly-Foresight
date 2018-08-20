f2a59a0390ad33d2dea6380506ba49388bc9250b
Pascal Grosset <pascal.av.grosset@gmail.com>
2018-08-20 09:09:12 -0600

    Update main.cpp
    throughput was inverted!!!

--- src/main.cpp
+++ src/main.cpp
@@ -188,8 +188,8 @@ int main(int argc, char *argv[])
 			double max_abs_err = *std::max_element(abs_err.begin(), abs_err.end());
 			double compress_time = compressClock.getDuration();
 			double decompress_time = decompressClock.getDuration();
-			double compress_throughput = compress_time / (ioMgr->getNumElements() * ioMgr->getTypeSize());
-			double decompress_throughput = decompress_time / (ioMgr->getNumElements() * ioMgr->getTypeSize());
+			double compress_throughput = (ioMgr->getNumElements() * ioMgr->getTypeSize()) / compress_time;
+			double decompress_throughput = (ioMgr->getNumElements() * ioMgr->getTypeSize()) / decompress_time;
 
 			double total_max_rel_err = 0;
 			double total_max_abs_err = 0;
