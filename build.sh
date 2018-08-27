3cc6f198b59af51f83f87236ea7281d23c136e62
Pascal Grosset <pascal.av.grosset@gmail.com>
2018-08-27 09:32:43 -0600

    fixed build error

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash 
+
+cd ExternalDependencies
+source buildExternal.sh
+cd ..
  
 # Get parameter 
 if [ "$#" -ne 1 ]; then 
@@ -12,5 +16,5 @@ mkdir $buildDir
 cd $buildDir 
  
 # build 
-cmake ../src 
+cmake ../src -DBLOSC_INCLUDE_PATH=../ExternalDependencies/c-blosc/install/include -DBLOSC_LIBRARY=../ExternalDependencies/c-blosc/install/lib/libblosc.so -DSZ_INCLUDE_PATH=../ExternalDependencies/SZ/sz/include -DSZ_LIBRARY=../ExternalDependencies/SZ/install/lib/libSZ.so -DZLIB_LIBRARY=../ExternalDependencies/SZ/install/lib/libzlib.so -DZSTD_LIBRARY=../ExternalDependencies/SZ/install/lib/libzstd.so
 make -j16 
