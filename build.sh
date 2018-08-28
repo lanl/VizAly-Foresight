fd9ec766fde996248f5cdac2d1e0d462b3e844d4
Pascal Grosset <pascal.av.grosset@gmail.com>
2018-08-28 11:06:49 -0600

    bigcruch still wont work on cooley

--- build.sh
+++ build.sh
@@ -14,6 +14,6 @@ mkdir $buildDir
 cd $buildDir 
  
 # build 
-cmake ../src -DBLOSC_INCLUDE_PATH=$projectPath/ExternalDependencies/c-blosc/install/include -DBLOSC_LIBRARY=$projectPath/ExternalDependencies/c-blosc/install/lib/libblosc.so -DSZ_INCLUDE_PATH=$projectPath/ExternalDependencies/SZ/sz/include -DSZ_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libSZ.so -DZLIB_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzlib.so -DZSTD_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzstd.so -DBIGCRUNCH_INCLUDE_PATH=$projectPath/ExternalDependencies/VizAly-BigCrunch/include -DBIGCRUNCH_LIBRARY=$projectPath/ExternalDependencies/VizAly-BigCrunch/install/lib/libbigcrunch.a -DBIGCRUNCH_BLOSC_LIBRARY=$projectPath/ExternalDependencies/VizAly-BigCrunch/build/3rdparty/blosc/blosc-external/src/blosc-external-build/blosc/libblosc.a
+cmake ../src -DBLOSC_INCLUDE_PATH=$projectPath/ExternalDependencies/c-blosc/install/include -DBLOSC_LIBRARY=$projectPath/ExternalDependencies/c-blosc/install/lib/libblosc.so -DSZ_INCLUDE_PATH=$projectPath/ExternalDependencies/SZ/sz/include -DSZ_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libSZ.so -DZLIB_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzlib.so -DZSTD_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzstd.so
 make -j
 
