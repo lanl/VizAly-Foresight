ef57e6e53297868c9cae67bee86ed3e6f2721832
Jesus J. Pulido <pulido@lanl.gov>
2018-08-29 15:22:06 -0600

    Enabled BigCrunch on default build

--- build.sh
+++ build.sh
@@ -14,6 +14,14 @@ mkdir $buildDir
 cd $buildDir 
  
 # build 
-cmake ../src -DBLOSC_INCLUDE_PATH=$projectPath/ExternalDependencies/c-blosc/install/include -DBLOSC_LIBRARY=$projectPath/ExternalDependencies/c-blosc/install/lib/libblosc.so -DSZ_INCLUDE_PATH=$projectPath/ExternalDependencies/SZ/sz/include -DSZ_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libSZ.so -DZLIB_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzlib.so -DZSTD_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzstd.so
+cmake ../src -DBLOSC_INCLUDE_PATH=$projectPath/ExternalDependencies/c-blosc/install/include \
+	-DBLOSC_LIBRARY=$projectPath/ExternalDependencies/c-blosc/install/lib/libblosc.so \
+	-DSZ_INCLUDE_PATH=$projectPath/ExternalDependencies/SZ/sz/include \
+	-DSZ_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libSZ.so \
+	-DZLIB_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzlib.so \
+	-DZSTD_LIBRARY=$projectPath/ExternalDependencies/SZ/install/lib/libzstd.so \
+	-DBIGCRUNCH_INCLUDE_PATH=$projectPath/ExternalDependencies/VizAly-BigCrunch/install/include \
+	-DBIGCRUNCH_LIBRARY=$projectPath/ExternalDependencies/VizAly-BigCrunch/install/lib/libbigcrunch.so
+	
 make -j
 
