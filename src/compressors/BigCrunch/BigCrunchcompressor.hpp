45fc020cae420bb515d34fb40333544561b214f1
Pascal Grosset <pascal.av.grosset@gmail.com>
2018-08-29 21:16:40 -0600

    using ERR instead of ERROR in bigcrunch

--- src/compressors/BigCrunch/BigCrunchcompressor.hpp
+++ src/compressors/BigCrunch/BigCrunchcompressor.hpp
@@ -40,7 +40,7 @@ inline void BigCrunchCompressor::init()
 inline int BigCrunchCompressor::compress(void *input, void *&output, std::string dataType, size_t dataTypeSize, size_t n)
 {
 	// Default Params { Error:-3, Tolerance:1, BLOSC_NTHREADS: 1, BLOSCFILTER:SHUFFLE, BLOSC_COMPRESSOR:ZSTD }
-	bigcrunch::setting_t settings = { {bigcrunch::config_t::ERROR, -3},
+	bigcrunch::setting_t settings = { {bigcrunch::config_t::ERR, -3},
 				 {bigcrunch::config_t::TOLERANCE, 0},
 				 {bigcrunch::config_t::BLOSC_NTHREADS, 1},
 				 {bigcrunch::config_t::BLOSC_COMPRESSOR, bigcrunch::blosc_compressor_t::ZSTD},
@@ -67,7 +67,7 @@ inline int BigCrunchCompressor::compress(void *input, void *&output, std::string
 
 inline int BigCrunchCompressor::decompress(void *&input, void *&output, std::string dataType, size_t dataTypeSize, size_t n)
 {
-	bigcrunch::setting_t settings = { {bigcrunch::config_t::ERROR, -3},
+	bigcrunch::setting_t settings = { {bigcrunch::config_t::ERR, -3},
 				 {bigcrunch::config_t::TOLERANCE, 1},
 				 {bigcrunch::config_t::BLOSC_NTHREADS, 1},
 				 {bigcrunch::config_t::BLOSC_COMPRESSOR, bigcrunch::blosc_compressor_t::ZSTD},
