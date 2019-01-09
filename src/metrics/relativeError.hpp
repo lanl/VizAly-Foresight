673083ac87b6a32d9af227a03b724da083383100
Jesus Pulido <jpulidojr@gmail.com>
2019-01-09 14:52:35 -0700

    fixed global psnr and added more debug metrics

--- src/metrics/relativeError.hpp
+++ src/metrics/relativeError.hpp
@@ -68,11 +68,13 @@ inline T relError(T original, T approx, double tolerance)
 inline void relativeError::execute(void *original, void *approx, size_t n) {
 	std::vector<double> rel_err(n);
 
+    double sum_rel_err = 0;
 	for (std::size_t i = 0; i < n; ++i)
 	{
 		// Max set tolerence to 1
 		double err = relError(static_cast<float *>(original)[i], static_cast<float *>(approx)[i], 1);
 		rel_err.push_back(err);
+        sum_rel_err += err;
 	}
 	double max_rel_err = *std::max_element(rel_err.begin(), rel_err.end());
 	val = max_rel_err;
@@ -81,7 +83,21 @@ inline void relativeError::execute(void *original, void *approx, size_t n) {
 	MPI_Allreduce(&max_rel_err, &total_max_rel_err, 1, MPI_DOUBLE, MPI_MAX, comm);// MPI_COMM_WORLD);
 	total_val = total_max_rel_err;
 
-	log << " Max Rel Error: " << total_max_rel_err << std::endl;
+	log << "-Max Rel Error: " << total_max_rel_err << std::endl;
+
+    // Additional debug metrics, only in run_log
+    // Global total sum of error
+    double glob_sum_rel_err = 0;
+    MPI_Allreduce(&sum_rel_err, &glob_sum_rel_err, 1, MPI_DOUBLE, MPI_SUM, comm);
+
+    // Global number of values
+    size_t global_n = 0;
+    MPI_Allreduce(&n, &global_n, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, comm);
+
+    // Compute mean
+    double mean_rel_err = glob_sum_rel_err / global_n;
+    log << " Total Rel Error: " << glob_sum_rel_err << std::endl;
+    log << " Mean Rel Error: " << mean_rel_err << std::endl;
 
 	MPI_Barrier(comm);
 	return;
