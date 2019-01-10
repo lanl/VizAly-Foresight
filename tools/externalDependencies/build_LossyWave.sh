bb570d3344a206264f788cac2596d4034f7c7740
Jesus Pulido <jpulidojr@gmail.com>
2019-01-10 16:35:54 -0700

    added LossyWave autobuild support

---
+++ tools/externalDependencies/build_LossyWave.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# LossyWave
+echo "Building LossyWave ... "
+
+git clone https://github.com/lanl/VizAly-LossyWave.git
+cd VizAly-LossyWave/
+#git checkout v1.1
+mkdir install
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../install -DLOSSYWAVE_BUILD_STATIC:BOOL=false
+make -j 
+make install
+cd ..
+cd ..
+
+echo "Building LossyWave done!"
