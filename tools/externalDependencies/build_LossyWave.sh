d3852c49a86563f5efc829a5fc706a18e8414356
Jesus Pulido <jpulidojr@gmail.com>
2019-01-18 14:31:59 -0700

    updated tag version

--- tools/externalDependencies/build_LossyWave.sh
+++ tools/externalDependencies/build_LossyWave.sh
@@ -5,7 +5,7 @@ echo "Building LossyWave ... "
 
 git clone https://github.com/lanl/VizAly-LossyWave.git
 cd VizAly-LossyWave/
-git checkout v0.1
+git checkout v0.2
 mkdir install
 mkdir build
 cd build
