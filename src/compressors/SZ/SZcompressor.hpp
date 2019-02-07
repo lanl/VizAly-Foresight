1764b0d9f12eb8ee6855f16f167eca6ea591b511
Jesus Pulido <jpulidojr@gmail.com>
2019-02-07 13:43:29 -0700

    sz parsing updates, can now change modes

--- src/compressors/SZ/SZcompressor.hpp
+++ src/compressors/SZ/SZcompressor.hpp
@@ -47,26 +47,37 @@ inline int SZCompressor::compress(void *input, void *&output, std::string dataTy
 	Timer cTime; cTime.start();
 	SZ_Init(NULL); 
 
+	int mode = PW_REL; // Default by Sheng, PW_REL = 10
+
 	double relTol = 1E-3;
 	std::unordered_map<std::string, std::string>::const_iterator got = compressorParameters.find("tolerance");
 	if( got != compressorParameters.end() )
 		if (compressorParameters["tolerance"] != "")
-			relTol = strConvert::to_double( compressorParameters["tolerance"] );
+		{
+			relTol = strConvert::to_double(compressorParameters["tolerance"]);
+			mode = PW_REL;
+		}
 
-        double lowAbs = 0.0;
-        got = compressorParameters.find("lower");
-        if( got != compressorParameters.end() )
-                if (compressorParameters["lower"] != "")
-                        lowAbs = strConvert::to_double( compressorParameters["lower"] );
+	double absTol = 0.0;
+	got = compressorParameters.find("abs");
+	if( got != compressorParameters.end() )
+		if (compressorParameters["abs"] != "")
+		{
+			absTol = strConvert::to_double(compressorParameters["abs"]);
+			mode = ABS;
+		}
 
-        double upperAbs = 0.0;
-        got = compressorParameters.find("upper");
-        if( got != compressorParameters.end() )
-                if (compressorParameters["upper"] != "")
-                        upperAbs = strConvert::to_double( compressorParameters["upper"] );
+	double powerTol = 0.0;
+	got = compressorParameters.find("power");
+	if( got != compressorParameters.end() )
+		if (compressorParameters["power"] != "")
+		{
+			powerTol = strConvert::to_double(compressorParameters["power"]);
+			// Unknown mode, just fill in input to SZ
+		}
 
 	std::uint64_t csize = 0;
-	std::uint8_t *cdata = SZ_compress_args(SZ_FLOAT, static_cast<float *>(input), &csize, PW_REL, lowAbs, upperAbs, relTol, n[4], n[3], n[2], n[1], n[0]);
+	std::uint8_t *cdata = SZ_compress_args(SZ_FLOAT, static_cast<float *>(input), &csize, mode, absTol, powerTol, relTol, n[4], n[3], n[2], n[1], n[0]);
 	
 	output = cdata;
 	cTime.stop();
