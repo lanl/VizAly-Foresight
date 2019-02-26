2ed37ba6ba5a79e851f8ad31779ddb28d29a263d
Pascal Grosset <pascal.av.grosset@gmail.com>
2019-02-26 10:36:48 -0700

    Fixed types and missing info

--- scripts/darwinScalingJobCreator.sh
+++ scripts/darwinScalingJobCreator.sh
@@ -7,8 +7,9 @@ inputPath=$4
 
 echo "#!/bin/bash" > temp.sh
 echo "#SBATCH -N $numNodes"         >> temp.sh
-echo "#SBATCH --ntasks-per-node 8"  >> temp.sh
+echo "#SBATCH --ntasks-per-node $ranksPerNode"  >> temp.sh
 echo "#SBATCH -p scaling"           >> temp.sh
+echo "#SBATCH -qos=scaling"         >> temp.sh
 
 echo "source $projectPath/scripts/VizAly-CBench.bash.darwin"    >> temp.sh
 echo "cd $projectPath/build"                                    >> temp.sh
@@ -18,7 +19,7 @@ sbatch temp.sh
 rm temp.sh
 
 # Run as:
-# dawinJobCreator.sh 8 8 /projects/exasky/VizAly-CBench/ ../inputs/darwinStep499.json
+# darwinScalingJobCreator.sh 8 8 /projects/exasky/VizAly-CBench ../inputs/darwinStep499.json
 # Param 1: num nodes
 # Param 2: num ranks per node
 # Param 3: project path
