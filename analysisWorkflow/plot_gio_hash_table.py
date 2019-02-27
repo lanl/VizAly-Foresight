76dee6d91d19e82f00c1dac8781c7e902768475e
Christopher Michael Biwer <cmbiwer@lanl.gov>
2019-02-27 16:23:11 -0700

    Initial script.

---
+++ analysisWorkflow/plot_gio_hash_table.py
@@ -0,0 +1,218 @@
+#! /usr/bin/env python
+
+import argparse
+import gioSqlite as gio_sqlite
+import matplotlib.pyplot as plt
+import numpy
+import os
+
+def make_fake_data():
+
+    # particles
+    n_particles = 1e5
+
+    # random sampling coordinates
+    x_min, x_max = 0.0, 256.0
+    mass_min, mass_max = 1e13, 1e14
+    vx_min, vx_max = 2e2, 1e3
+    coords_mins = [x_min, x_min, x_min, mass_min, vx_min, vx_min, vx_min]
+    coords_maxs = [x_max, x_max, x_max, mass_max, vx_max, vx_max, vx_max]
+    coords_raw = []
+    for coord_min, coord_max in zip(coords_mins, coords_maxs):
+        coords_raw.append(numpy.random.uniform(coord_min, coord_max, n_particles))
+    coords_raw = numpy.vstack(coords_raw)
+    
+    # add noise
+    x_mean = 0.0
+    x_var = 0.003
+    mass_mean = 1e6
+    mass_var = 1e3
+    vx_mean = None
+    vx_var = None
+    jit_means = [x_mean, x_mean, x_mean, mass_mean, vx_mean, vx_mean, vx_mean]
+    jit_vars = [x_var, x_var, x_var, mass_var, vx_var, vx_var, vx_var]
+    coords_jit = []
+    for i, (jit_mean, jit_var) in enumerate(zip(jit_means, jit_vars)):
+        if jit_mean == jit_var == None:
+            vals = coords_raw[i, :]
+        else:
+            vals = coords_raw[i, :] + numpy.random.normal(jit_mean, jit_var, n_particles)
+        coords_jit.append(vals)
+    coords_jit = numpy.vstack(coords_jit)
+    
+    # example data 1
+    data_1 = coords_raw
+    
+    # example data 2
+    data_2 = coords_jit
+
+    return data_1, data_2
+
+def load_sqlite_data(path, query, sqlite_file):
+
+    # load file
+    print("Reading {}...".format(path))
+    query_mgr = gio_sqlite.GioSqlite3()
+    query_mgr.loadGIOSqlite(sqlite_file)
+
+    # load data
+    i = 0
+    table_name = "foo_{}".format(i)
+    query_mgr.createTable(table_name, (path))
+
+    # execute query
+    query = query.replace("__TABLE__", table_name)
+    result = query_mgr.runQueryOutputList(query)
+
+    # typecast
+    result = numpy.array(result)
+
+    return result
+
+def snap(x, d=0.25):
+#    d = 1.0 / d
+#    return numpy.ceil(d * x) / d
+    return numpy.ceil(x / d) * d
+
+# parse command line
+parser = argparse.ArgumentParser()
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--parameters", nargs="+", default=["fof_halo_center_x", "fof_halo_center_y", "fof_halo_center_z", "fof_halo_mass"])
+parser.add_argument("--match-parameters", nargs="+", default=["fof_halo_center_x"])
+parser.add_argument("--match-parameters-snaps", nargs="+", type=float, default=[-1])
+parser.add_argument("--order-by-parameter", default="fof_halo_mass")
+parser.add_argument("--input-file")
+parser.add_argument("--reference-file")
+parser.add_argument("--output-file-format")
+parser.add_argument("--sqlite-file", default="/home/cmbiwer/src/VizAly-Vis_IO/genericio/frontend/GenericIOSQLite.so")
+parser.add_argument("--limit", type=float, default=None)
+parser.add_argument("--bins", type=int, default=50)
+opts = parser.parse_args()
+
+# add parameters
+for p in opts.match_parameters:
+    if p not in opts.parameters:
+        opts.parameters.append(p)
+
+# create query
+#query = "select {} from __TABLE__ ORDER BY {} DESC".format(", ".join(opts.parameters), opts.order_by_parameter)
+query = "select {} from __TABLE__".format(", ".join(opts.parameters))
+if opts.limit:
+    query += " LIMIT {}".format(opts.limit)
+
+# set seed
+numpy.random.seed(opts.seed)
+
+# count scalar fields to query and match
+n_parameters = len(opts.parameters)
+assert(len(opts.match_parameters) == len(opts.match_parameters_snaps))
+
+# determine index of match parameters
+match_parameters_idxs = []
+for p in opts.match_parameters:
+    match_parameters_idxs.append(opts.parameters.index(p))
+
+# set data to compare
+if opts.input_file and opts.reference_file:
+    data_1 = load_sqlite_data(opts.reference_file, query, opts.sqlite_file)
+    data_2 = load_sqlite_data(opts.input_file, query, opts.sqlite_file)
+    data_1 = numpy.transpose(data_1)
+    data_2 = numpy.transpose(data_2)
+elif opts.input_file or opts.reference_file:
+    raise KeyError("Must use both --input-file and --reference-file!")
+else:
+    data_1, data_2 = make_fake_data()
+
+# print statment
+print("Read {} particles from {}...".format(data_1.shape[1], opts.input_file))
+print("Read {} particles from {}...".format(data_2.shape[1], opts.reference_file))
+
+# snap
+data_1_snap = []
+data_2_snap = []
+for i, d in zip(match_parameters_idxs, opts.match_parameters_snaps):
+    if d == -1:
+        data_1_snap.append(data_1[i, :])
+        data_2_snap.append(data_2[i, :])
+    else:
+        data_1_snap.append(snap(data_1[i, :], d))
+        data_2_snap.append(snap(data_2[i, :], d))
+    print(data_1[i, :])
+    print(data_1_snap[-1])
+data_1_snap = numpy.vstack(data_1_snap)
+data_2_snap = numpy.vstack(data_2_snap)
+
+# transpose for loop
+data_1_snap = numpy.transpose(data_1_snap)
+data_2_snap = numpy.transpose(data_2_snap)
+data_1_snap[data_1_snap == -0.0] = 0.0
+data_2_snap[data_2_snap == -0.0] = 0.0
+
+print
+print(data_1)
+print(data_2)
+print(data_1_snap)
+print(data_2_snap)
+
+# hash
+n_lost = 0
+diffs = []
+lost_idxs = []
+for i, d_1 in enumerate(data_1_snap):
+
+    # find matches
+    idx = numpy.flatnonzero((d_1 == data_2_snap).all(1))
+
+    # ensure unqiue matches
+    # record number of particles without match
+    if idx.size > 1:
+        #print("Too many FOUND!", i, idx.size)
+        n_lost += 1
+        continue
+#        raise KeyError("Mapping to more than one particle!")
+    elif idx.size == 0:
+        n_lost += 1
+        lost_idxs.append(i)
+        #print("Not FOUND!", i, idx)
+        continue
+    else:
+        idx = idx[0]
+
+    # get hashed values
+    d_2 = data_2_snap[idx]
+
+    # get data values
+    r_1 = data_1[:, i]
+    r_2 = data_2[:, idx]
+
+    # compute differences
+    diff = r_2 - r_1
+    diff_hash = d_2 - d_1
+
+    # append result
+    diffs.append(diff)
+
+# typecast and count
+diffs = numpy.vstack(diffs)
+n_found = diffs.shape[0]
+n_particles = data_1.shape[1]
+
+# print statement
+print("Number of matched particles: {}".format(n_found))
+print("Number of missed particles: {}".format(n_lost))
+print("Indices missed were: {}".format(lost_idxs))
+#for lost_idx in lost_idxs:
+#    print()
+#    print(data_1[:, lost_idx])
+#    print(data_1_snap[lost_idx, :])
+assert(n_found + n_lost == n_particles)
+
+# plot
+for i in range(n_parameters):
+    plt.hist(diffs[:, i], bins=opts.bins)
+    plt.xlabel("Difference {}".format(opts.parameters[i]))
+    if opts.output_file_format:
+        plt.savefig(opts.output_file_format.format(i))
+    else:
+        plt.show()
+    plt.close()
