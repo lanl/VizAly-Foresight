14cc363fc67474ca15522a61c242eb8db72f2892
Christopher Michael Biwer <cmbiwer@lanl.gov>
2019-02-27 16:45:06 -0700

    Update.

--- analysisWorkflow/plot_gio_hash_table.py
+++ analysisWorkflow/plot_gio_hash_table.py
@@ -8,6 +8,16 @@ import matplotlib.pyplot as plt
 import numpy
 import os
 
+def operate(y, ref, operation):
+    """ Suite of math operations for plot.
+    """
+    if operation == "diff":
+        return y - ref
+    elif operation == "ratio":
+        return y / ref
+    else:
+        raise NotImplementedError("Do not understand operation {}!".format(operation))
+
 def load_sqlite_data(path, query, sqlite_file):
     """ Loads data using SQLite query.
     """
@@ -38,16 +48,17 @@ def snap(x, d=0.25):
 
 # parse command line
 parser = argparse.ArgumentParser()
-parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--input-file", required=True)
+parser.add_argument("--reference-file", required=True)
+parser.add_argument("--output-file")
+parser.add_argument("--sqlite-file", default="/home/cmbiwer/src/VizAly-Vis_IO/genericio/frontend/GenericIOSQLite.so")
 parser.add_argument("--parameters", nargs="+", default=["fof_halo_center_x", "fof_halo_center_y", "fof_halo_center_z", "fof_halo_mass"])
 parser.add_argument("--match-parameters", nargs="+", default=["fof_halo_center_x"])
 parser.add_argument("--match-parameters-snaps", nargs="+", type=float, default=[-1])
-parser.add_argument("--order-by-parameter", default="fof_halo_mass")
-parser.add_argument("--input-file")
-parser.add_argument("--reference-file")
-parser.add_argument("--output-file-format")
-parser.add_argument("--sqlite-file", default="/home/cmbiwer/src/VizAly-Vis_IO/genericio/frontend/GenericIOSQLite.so")
+parser.add_argument("--order-by")
+parser.add_argument("--descending", action="store_true")
 parser.add_argument("--limit", type=float, default=None)
+parser.add_argument("--operation", choices=["diff", "ratio"], default="diff")
 parser.add_argument("--bins", type=int, default=50)
 opts = parser.parse_args()
 
@@ -57,13 +68,15 @@ for p in opts.match_parameters:
         opts.parameters.append(p)
 
 # create query
-#query = "select {} from __TABLE__ ORDER BY {} DESC".format(", ".join(opts.parameters), opts.order_by_parameter)
 query = "select {} from __TABLE__".format(", ".join(opts.parameters))
+if opts.order_by:
+    query += " ORDER BY {}".format(opts.order_by)
+if opts.descending:
+    query += " DESC"
 if opts.limit:
     query += " LIMIT {}".format(opts.limit)
 
-# set seed
-numpy.random.seed(opts.seed)
+print(query)
 
 # count scalar fields to query and match
 n_parameters = len(opts.parameters)
@@ -97,8 +110,6 @@ for i, d in zip(match_parameters_idxs, opts.match_parameters_snaps):
     else:
         data_1_snap.append(snap(data_1[i, :], d))
         data_2_snap.append(snap(data_2[i, :], d))
-    print(data_1[i, :])
-    print(data_1_snap[-1])
 data_1_snap = numpy.vstack(data_1_snap)
 data_2_snap = numpy.vstack(data_2_snap)
 
@@ -108,15 +119,9 @@ data_2_snap = numpy.transpose(data_2_snap)
 data_1_snap[data_1_snap == -0.0] = 0.0
 data_2_snap[data_2_snap == -0.0] = 0.0
 
-print
-print(data_1)
-print(data_2)
-print(data_1_snap)
-print(data_2_snap)
-
 # hash
 n_lost = 0
-diffs = []
+metrics = []
 lost_idxs = []
 for i, d_1 in enumerate(data_1_snap):
 
@@ -126,14 +131,11 @@ for i, d_1 in enumerate(data_1_snap):
     # ensure unqiue matches
     # record number of particles without match
     if idx.size > 1:
-        #print("Too many FOUND!", i, idx.size)
         n_lost += 1
         continue
-#        raise KeyError("Mapping to more than one particle!")
     elif idx.size == 0:
         n_lost += 1
         lost_idxs.append(i)
-        #print("Not FOUND!", i, idx)
         continue
     else:
         idx = idx[0]
@@ -145,34 +147,33 @@ for i, d_1 in enumerate(data_1_snap):
     r_1 = data_1[:, i]
     r_2 = data_2[:, idx]
 
-    # compute differences
-    diff = r_2 - r_1
-    diff_hash = d_2 - d_1
+    # compute metric
+    metric = operate(r_2, r_1, opts.operation)
+    metric_hash = operate(d_2, d_1, opts.operation)
 
     # append result
-    diffs.append(diff)
+    metrics.append(metric)
 
 # typecast and count
-diffs = numpy.vstack(diffs)
-n_found = diffs.shape[0]
+metrics = numpy.vstack(metrics)
+n_found = metrics.shape[0]
 n_particles = data_1.shape[1]
 
 # print statement
 print("Number of matched particles: {}".format(n_found))
 print("Number of missed particles: {}".format(n_lost))
 print("Indices missed were: {}".format(lost_idxs))
-#for lost_idx in lost_idxs:
-#    print()
-#    print(data_1[:, lost_idx])
-#    print(data_1_snap[lost_idx, :])
 assert(n_found + n_lost == n_particles)
 
 # plot
 for i in range(n_parameters):
-    plt.hist(diffs[:, i], bins=opts.bins)
+    plt.hist(metrics[:, i], bins=opts.bins)
     plt.xlabel("Difference {}".format(opts.parameters[i]))
-    if opts.output_file_format:
-        plt.savefig(opts.output_file_format.format(i))
+    if opts.output_file and i == 0:
+        if n_parameters > 1:
+            print("Will only write out {} into {}!".format(opts.parameters[i], opts.output_file))
+        plt.savefig(opts.output_file)
+        break
     else:
         plt.show()
     plt.close()
