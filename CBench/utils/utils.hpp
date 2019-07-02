5ccab01545c83a8fdc75b2a399afbcbe22f23cd4
Jesus Pulido <jpulidojr@gmail.com>
2019-07-02 14:55:51 -0600

    made directory creation more robust

--- CBench/utils/utils.hpp
+++ CBench/utils/utils.hpp
@@ -6,6 +6,7 @@ All rights reserved.
 
 Authors:
  - Pascal Grosset
+ - Jesus Pulido
 ================================================================================*/
 #pragma once
 
@@ -21,18 +22,30 @@ Authors:
 #define mkdir(a, b) _mkdir(a)
 #endif
 
+struct stat finfo;
+
 inline int createFolder(std::string folderName)
 {
 #ifdef _WIN32
 	folderName = ".\\" + folderName;
 #endif
-    const int dir_err = mkdir(folderName.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-    if (-1 == dir_err)
-    {
-        std::cout << "Could not create directory " << folderName << std::endl;
-        return 0;
-    }
 
+	if (finfo.st_mode & S_IFDIR)
+		return 1; // Directory already exists 
+	else if (stat(folderName.c_str(), &finfo) != 0)
+	{
+		const int dir_err = mkdir(folderName.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+		if (-1 == dir_err)
+		{
+			std::cout << "Could not create directory " << folderName << std::endl;
+			return 0;
+		}
+	}
+	else 
+	{
+		//std::cout << folderName << " is not a directory!" << std::endl;
+		return 0;
+	}
     return 1;
 }
 
