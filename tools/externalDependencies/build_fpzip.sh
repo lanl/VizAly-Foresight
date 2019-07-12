53f4f545fab1f5c98004982e8e512950f5979cba
Jesus Pulido <jpulidojr@gmail.com>
2019-07-12 11:23:42 -0600

    fixed to file link instead of text link

--- tools/externalDependencies/build_fpzip.sh
+++ tools/externalDependencies/build_fpzip.sh
@@ -4,7 +4,7 @@
 echo "Building fpzip ... "
 
 #wget https://computing.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
-wget https://github.com/lanl/VizAly-Foresight/blob/gh-pages/compressors/fpzip-1.2.0.tar.gz?raw=true
+wget https://lanl.github.io/VizAly-Foresight/compressors/fpzip-1.2.0.tar.gz
 
 tar -zxvf fpzip-1.2.0.tar.gz
 cd fpzip-1.2.0
