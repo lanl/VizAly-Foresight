52ad1a067371022165bb945c36e61c93b11e3052
Jesus Pulido <jpulidojr@gmail.com>
2019-07-12 10:59:44 -0600

    updating existing link to auto-redirect link

--- tools/externalDependencies/build_fpzip.sh
+++ tools/externalDependencies/build_fpzip.sh
@@ -3,7 +3,8 @@
 # BigCrunch
 echo "Building fpzip ... "
 
-wget https://computation.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
+#wget https://computation.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
+wget https://computing.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
 tar -zxvf fpzip-1.2.0.tar.gz
 cd fpzip-1.2.0
 cd src
