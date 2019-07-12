acb070d6d237a326b6963fdc74dcae76a875a236
Jesus Pulido <jpulidojr@gmail.com>
2019-07-12 11:29:52 -0600

    updating external compressor links (#57)
    * updating existing link to auto-redirect link
    
    * temporary redirection
    
    * fixed direct link to githubio

--- tools/externalDependencies/build_fpzip.sh
+++ tools/externalDependencies/build_fpzip.sh
@@ -3,7 +3,9 @@
 # BigCrunch
 echo "Building fpzip ... "
 
-wget https://computation.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
+#wget https://computing.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
+wget https://lanl.github.io/VizAly-Foresight/compressors/fpzip-1.2.0.tar.gz
+
 tar -zxvf fpzip-1.2.0.tar.gz
 cd fpzip-1.2.0
 cd src
