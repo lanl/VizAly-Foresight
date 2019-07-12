1c2285d54ccafc5a981262e7eeb7c196f565d524
Jesus Pulido <jpulidojr@gmail.com>
2019-07-12 11:17:36 -0600

    direct link

--- tools/externalDependencies/build_fpzip.sh
+++ tools/externalDependencies/build_fpzip.sh
@@ -4,9 +4,9 @@
 echo "Building fpzip ... "
 
 #wget https://computing.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
-wget https://github.com/lanl/VizAly-Foresight/blob/gh-pages/compressors/fpzip-1.2.0.tar.gz
+wget https://github.com/lanl/VizAly-Foresight/blob/gh-pages/compressors/fpzip-1.2.0.tar.gz?raw=true
 
-tar -xvf fpzip-1.2.0.tar.gz
+tar -zxvf fpzip-1.2.0.tar.gz
 cd fpzip-1.2.0
 cd src
 make CXXFLAGS+='-fPIC' -j
