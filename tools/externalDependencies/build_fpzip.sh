f0cb830fc5c49013b5897fcf86bf6c5da99b242a
Jesus Pulido <jpulidojr@gmail.com>
2019-07-12 10:49:20 -0600

    Revert "Update build_fpzip.sh"
    This reverts commit 33227e39fde50dd95a4d2085a51b842e0bc01299.

--- tools/externalDependencies/build_fpzip.sh
+++ tools/externalDependencies/build_fpzip.sh
@@ -3,8 +3,7 @@
 # BigCrunch
 echo "Building fpzip ... "
 
-#wget https://computation.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
-wget https://github.com/lanl/VizAly-Foresight/blob/gh-pages/compressors/fpzip-1.2.0.tar.gz
+wget https://computation.llnl.gov/projects/floating-point-compression/download/fpzip-1.2.0.tar.gz
 tar -zxvf fpzip-1.2.0.tar.gz
 cd fpzip-1.2.0
 cd src
