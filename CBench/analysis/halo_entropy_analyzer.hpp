fc6371a6adfc7a217f6d1185f50f0d004b182cfa
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-07-25 15:23:27 -0600

    Dump data distribution and generate plot script

--- CBench/analysis/halo_entropy_analyzer.hpp
+++ CBench/analysis/halo_entropy_analyzer.hpp
@@ -39,14 +39,17 @@ public:
   ~HaloEntropy() = default;
  
   bool run();  // ok
-  bool computeFrequencies(std::string scalar, void* original, void* approx, size_t n);
-  double computeShannonEntropy(std::string scalar, size_t n);
+  bool computeFrequencies(std::string scalar, void* original, void* approx);
+  double computeShannonEntropy(std::string scalar);
   bool generateHistogram(std::string path) const;
+  void exportResults(std::string path = ".");
+  std::string extractBase(std::string path);
 
 private:
   // IO
-  std::string json_path = "";                                        // JSON parameter file path
-  std::unique_ptr<DataLoaderInterface> ioMgr;                        // HACC data loader
+  std::string json_path = "";
+  std::string input_hacc = "";
+  std::unique_ptr<DataLoaderInterface> ioMgr;
   std::stringstream debug_log;
 
   // bins partition
@@ -54,9 +57,7 @@ private:
   // per halo attribute data
   std::vector<std::string> attributes;                               // [x,y,x,vx,vy,vx]
   std::unordered_map<std::string, std::vector<double>> frequency;
-  std::unordered_map<std::string, std::string> type;                 // datatype of each attribute [float|double]
-  std::unordered_map<std::string, int> size;                         // size of each attribute dataset (in bytes)
-  std::unordered_map<std::string, int> nb_elems;                     // number of dataset elements for each attribute
+  std::unordered_map<std::string, size_t> size;                         // size of each attribute dataset (in bytes)
 
   // MPI
   int my_rank  = 0;                                                   // current MPI rank
@@ -73,7 +74,10 @@ inline HaloEntropy::HaloEntropy(const char* in_path, int in_rank,
 {}
 
 /* -------------------------------------------------------------------------- */
-inline bool HaloEntropy::computeFrequencies(std::string scalar, void* original, void* approx, size_t n) {
+inline bool HaloEntropy::computeFrequencies(std::string scalar, void* original, void* approx) {
+
+  double const& n = size[scalar];
+  assert(n > 0.);
 
   // step 1. determine lower and upper bounds on data
   double global_max = 0;
@@ -116,13 +120,7 @@ inline bool HaloEntropy::computeFrequencies(std::string scalar, void* original, 
       double relative_value = (value - global_min) / range;
       int index = (range * relative_value) / capacity;
 
-      /*
-      debug_log << "value: " << value
-                << ", relative_value: " << relative_value
-                << ", global_max: " << global_max << std::endl;*/
-      if (index >= num_bins) {
-        index--;
-      }
+      if (index >= num_bins) { index--; }
 
       local_histogram[index]++;
     }
@@ -144,22 +142,110 @@ inline bool HaloEntropy::computeFrequencies(std::string scalar, void* original, 
 }
 
 /* -------------------------------------------------------------------------- */
-inline double HaloEntropy::computeShannonEntropy(std::string scalar, size_t n) {
-
-  auto const& distribution = frequency[scalar];
+inline double HaloEntropy::computeShannonEntropy(std::string scalar) {
 
   double entropy = 0.;
-  for (auto&& p_i : distribution) {
+  double const epsilon = std::numeric_limits<double>::epsilon();
+
+  for (auto&& value : frequency[scalar]) {
+    auto const& p_i = std::max(value, epsilon);
     entropy += (p_i * std::log2(p_i));
   }
   entropy *= -1;
 
-  debug_log << "Shannon entropy for "<< scalar << ", using "
-            << num_bins << " bins is " << entropy
-            << ", accuracy: " << num_bins / static_cast<double>(n)
-            << std::endl;
+  debug_log << "shannon entropy for '"<< scalar << "' = "<< entropy
+           << " using " << num_bins << " bins." << std::endl;
 
   return entropy;
+}
+
+/* -------------------------------------------------------------------------- */
+inline std::string HaloEntropy::extractBase(std::string path) {
+  char sep = '/';
+#ifdef _WIN32
+  sep = '\\';
+#endif
+
+  size_t i = path.rfind(sep, path.length());
+  if (i != std::string::npos) {
+    return std::string(path.substr(i+1, path.length() - i));
+  }
+  return std::string("");
+}
+
+/* -------------------------------------------------------------------------- */
+inline void HaloEntropy::exportResults(std::string root_path) {
+
+  if (my_rank != 0)
+    return;
+
+  // dump data first
+  for (auto&& scalar : attributes) {
+    std::string path = root_path + "/distrib_" + scalar + ".dat";
+    std::ofstream file(path, std::ios::out|std::ios::trunc);
+    assert(file.is_open());
+    assert(file.good());
+
+    file << "# scalar: " << scalar << std::endl;
+    file << "# num_bins: " << num_bins << std::endl;
+    for (auto&& value : frequency[scalar]) {
+      file << value << std::endl;
+    }
+
+    file.close();
+  }
+
+  // generate gnuplot script then
+  std::string path = root_path + "/distrib.gnu";
+  std::ofstream file(path, std::ios::out);
+  assert(file.is_open());
+  assert(file.good());
+
+  file << "#" << std::endl;
+  file << "# HACC data distribution gnuplot script" << std::endl;
+  file << "#" << std::endl;
+
+  file << "reset" << std::endl;
+  file << "set terminal postscript eps enhanced color 14 size 21cm,14cm" << std::endl;
+  file << "set output 'distribution.eps'" << std::endl;
+  file << std::endl;
+  file << "set multiplot layout 2,3 title 'HACC particle data - ";
+  file << num_bins << " bins - file: " << extractBase(input_hacc) << "'" << std::endl;
+
+  for (auto&& scalar : attributes) {
+    // get bounds
+    double v_max = 0.;
+    for (auto&& value : frequency[scalar]) {
+      if (value > v_max) { v_max = value; }
+    }
+
+    double const entropy = computeShannonEntropy(scalar);
+
+    file << std::endl;
+    file << "# ----------------------------------------------------" << std::endl;
+    file << "set title '"<< scalar << ", entropy=" << entropy <<"'"<< std::endl;
+    file << "set size ratio 1" << std::endl;
+    file << "set xlabel 'particle values'" << std::endl;
+    file << "set ylabel 'frequency'" << std::endl;
+    file << std::endl;
+    file << "n     = 40" << std::endl;
+    file << "max   = "<< v_max << std::endl;
+    file << "min   = 0" << std::endl;
+    file << "width = (max-min)/(2*n)" << std::endl;
+    file << "# function used to map a value to the intervals" << std::endl;
+    file << "hist(x,width) = width * floor(x/width) + width/2.0" << std::endl;
+    file << std::endl;
+    file << "set xrange [min:max]" << std::endl;
+    file << "set yrange [0:]" << std::endl;
+    file << "set boxwidth 0.5*width" << std::endl;
+    file << "set style fill transparent solid 0.4" << std::endl;
+    file << "set grid, xtics, ytics" << std::endl;
+    file << "plot 'distrib_"<< scalar <<".dat' using (hist($1,width)):(1.0)";
+    file << " lc rgb '#800080' smooth freq with boxes notitle" << std::endl;
+  }
+  file << std::endl;
+  file << "unset multiplot" << std::endl;
+  file.close();
 }
 
 /* -------------------------------------------------------------------------- */
@@ -185,7 +271,7 @@ inline bool HaloEntropy::run() {
 
   // retrieve input file
   std::string filetype = json["input"]["filetype"];
-  std::string input_hacc = json["input"]["filename"];
+  input_hacc = json["input"]["filename"];
   if (filetype != "HACC") {
     if (my_rank == 0) 
       std::cerr << "Only HACC data is supported" << std::endl;
@@ -220,32 +306,27 @@ inline bool HaloEntropy::run() {
   // loop over all scalars and load each of them
   for (auto&& scalar: attributes) {
 
-    // for debug:
-    if (scalar != "x") continue;
-
     debug_log << "\nLoading and running " << scalar << std::endl;
+
     if (ioMgr->loadData(scalar)) {
       // save infos for debug
       debug_log << ioMgr->getDataInfo();
       debug_log << ioMgr->getLog();
-      // TODO: compute Shannon entropy distribution for this attribute
-      if (scalar == "x") {
-        auto const n = ioMgr->getNumElements();
-        computeFrequencies(scalar, ioMgr->data, ioMgr->data, n);
-        computeShannonEntropy(scalar, n);
-      }
+
+      size[scalar] = ioMgr->getNumElements();
+      computeFrequencies(scalar, ioMgr->data, ioMgr->data);
+      computeShannonEntropy(scalar);
 
     } else {
       if (my_rank == 0)
         std::cerr << "Error while loading " << scalar << ", exiting now" << std::endl;
       return false;
     }
-    //appendLog(output_log, log.str());
-    // wait for other ranks to complete
+
     MPI_Barrier(comm);
   }
 
-  // print some metadata on loaded HACC file
+  exportResults();
 
   // output logs
   std::ofstream logfile(output, std::ios::out);
