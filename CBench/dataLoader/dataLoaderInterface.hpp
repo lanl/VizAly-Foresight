b360fbfa62e8814e30719586f4f04fbdb656c9ad
Jesus Pulido <jpulidojr@gmail.com>
2019-08-22 13:01:11 -0600

    added more comments to dataloader class

--- CBench/dataLoader/dataLoaderInterface.hpp
+++ CBench/dataLoader/dataLoaderInterface.hpp
@@ -31,30 +31,30 @@ protected:
 	bool saveData;
 	int origNumDims;
 
-	size_t origDims[5]{ 0,0,0,0,0 };
-	size_t sizePerDim[5]{ 0,0,0,0,0 };	// For compression
-	size_t rankOffset[3];
-	size_t elemSize;				            // size in bytes of that parameter
-	size_t totalNumberOfElements;	      // total number of particles for input file
-	size_t numElements;				          // number of particles for that mpi rank
+	size_t origDims[5]{ 0,0,0,0,0 };	// Global dataset size
+	size_t sizePerDim[5]{ 0,0,0,0,0 };	// For compression, size of this mpi rank
+	size_t rankOffset[3];				// Rank offset, for parallel operations
+	size_t elemSize;					// sizeof() in bytes of that parameter
+	size_t totalNumberOfElements;		// total number of points/cells for input file
+	size_t numElements;					// number of points/cells for this mpi rank
 
-	MPI_Comm comm;
+	MPI_Comm comm;						// global mpi handler
 
-public:   // TO_CHANGE
-	void *data;
+public:   // TO_CHANGE, *data should not be public
+	void *data;							// raw data pointer (do not touch outside!!)
 	
-	std::unordered_map<std::string, std::string> loaderParams;
-	std::vector<GioData> inOutData;
+	std::unordered_map<std::string, std::string> loaderParams;  // data-specific parameters, used by binary reader
+	std::vector<GioData> inOutData;		// passthrough data, used by genericio reader
 
   public:
 	virtual void init(std::string _filename, MPI_Comm _comm) = 0;
-	virtual int loadData(std::string paramName) = 0;
-	virtual int saveCompData(std::string paramName, void * cData) = 0;
-	virtual int writeData(std::string _filename) = 0;
-	virtual int saveInputFileParameters() = 0;
+	virtual int loadData(std::string paramName) = 0;					// Read op
+	virtual int saveCompData(std::string paramName, void * cData) = 0;	// Stores the data-pointer for future writing
+	virtual int writeData(std::string _filename) = 0;					// Write op
+	virtual int saveInputFileParameters() = 0;							// Reads file header only and stores data properties
 	virtual int close() = 0;
 	virtual void setParam(std::string paramName, std::string type, std::string value) = 0;
-  virtual bool loadUncompressedFields(nlohmann::json const& jsonInput) = 0;
+	virtual bool loadUncompressedFields(nlohmann::json const& jsonInput) = 0; // Data passthrough, used by hdf5 reader
 
 	size_t getNumElements() { return numElements; }
 	size_t * getSizePerDim() { return sizePerDim; }
@@ -64,7 +64,7 @@ public:   // TO_CHANGE
 	std::string getDataInfo();
 	std::string getLog() { return log.str(); }
 
-	void setSave(bool state) { saveData = state; }
+	void setSave(bool state) { saveData = state; }	// if true, write out decomp data
 };
 
 
