86937ab0b2f2907166287e2e714745a14e51bab6
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-09-11 17:38:00 -0600

    Cleanup and update batch scripts

--- scripts/analysis/pk-comp.sh
+++ scripts/analysis/pk-comp.sh
@@ -7,9 +7,8 @@
 
 HACC="/projects/exasky/HACC"
 POWER_SPECTRUM="${HACC}/trunk/Darwin/mpi/bin/hacc_pk_gio_auto"
-#PARTICLES_DATA="/projects/exasky/hoby-projects/cbench/build/data/all_combined_m000-499.particles"
-PARTICLES_DATA="/projects/exasky/hoby-projects/cbench/build/data/all_hyper_compressed_m000-499.particles"
-OUTPUT_DATA="/projects/exasky/hoby-projects/cbench/build/data/analysis-spectrum-hypercomp.dat"
+PARTICLES_DATA="/projects/exasky/hoby-projects/cbench/build/data/data-combined-zip"
+OUTPUT_DATA="/projects/exasky/hoby-projects/cbench/build/data/pk-combined-zip.dat"
 TIMESTEP=499
 
 # load modules
