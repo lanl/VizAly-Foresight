93e723642841121f1f4f7e946b6bf999ccd757a1
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-09-16 15:04:23 -0600

    Add status check before moving on next step

--- scripts/analysis/pipeline.sh
+++ scripts/analysis/pipeline.sh
@@ -6,9 +6,9 @@
 #SBATCH --job-name analysis
 
 # enable or disable steps
-EXTRACT_NON_HALOS=true
-COMPRESS_NON_HALOS=false
-MERGE_DATASETS=false
+EXTRACT_NON_HALOS=false
+COMPRESS_NON_HALOS=true
+MERGE_DATASETS=true
 COMPUTE_POWER_SPECTRUM=false
 
 # parameters
@@ -17,31 +17,35 @@ TIMESTEP=499
 SUFFIX="sampled"
 HACC="/projects/exasky/HACC"
 BUILD="/projects/exasky/hoby-projects/cbench/build"
-INPUT_JSON="../inputs/hacc/analysis_pipeline-${SUFFIX}.json"
+INPUT_JSON="../inputs/hacc/analysis_pipeline_${SUFFIX}.json"
 POWER_SPECTRUM="${HACC}/trunk/Darwin/mpi/bin/hacc_pk_gio_auto"
 PARTICLES_DATA="/projects/exasky_data/hoby/analysis/data-combined-zip-${SUFFIX}"
 OUTPUT_DATA="/projects/exasky_data/hoby/analysis/pk-combined-zip-${SUFFIX}.dat"
+STATUS=0
 
 # extract non-halos and compute entropy if required
 if ${EXTRACT_NON_HALOS}; then
   source "/home/hoby/.bashrc" && cd ${BUILD} &&
   mpirun -np ${NRANKS} ./analyzer ${INPUT_JSON}
+  STATUS=$?
 fi
 
 # compress non-halo particles dataset
-if ${COMPRESS_NON_HALOS}; then
+if ${COMPRESS_NON_HALOS} && [ ${STATUS} -eq 0 ]; then
   source "${HACC}.darwin_setup" && cd ${BUILD} &&
   mpirun -np ${NRANKS} ./CBench ${INPUT_JSON}
+  STATUS=$?
 fi
 
 # merge it with halo ones
-if ${MERGE_DATASETS}; then
+if ${MERGE_DATASETS} && [ ${STATUS} -eq 0 ]; then
   source "/home/hoby/.bashrc" && cd ${BUILD} &&
   mpirun -np ${NRANKS} ./merger ${INPUT_JSON}
+  STATUS=$?
 fi
 
 # compute power spectrum eventually
-if ${COMPUTE_POWER_SPECTRUM}; then
+if ${COMPUTE_POWER_SPECTRUM} && [ ${STATUS} -eq 0 ]; then
   source "${HACC}.darwin_setup" && cd "${HACC}/run" &&
   mpirun ${POWER_SPECTRUM} inputs/indat.params -n ${PARTICLES_DATA} ${OUTPUT_DATA} ${TIMESTEP}
 fi
