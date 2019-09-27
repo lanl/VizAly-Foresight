ab83df79d1ef5d0386416d218c960e6077a3719d
Hoby Rakotoarivelo <hoby.rakotoarivelo@gmail.com>
2019-09-27 12:18:52 -0600

    Simplified batch script

--- scripts/analysis/pipeline.sh
+++ scripts/analysis/pipeline.sh
@@ -19,33 +19,30 @@ HACC="/projects/exasky/HACC"
 BUILD="/projects/exasky/hoby-projects/cbench/build"
 INPUT_JSON="../inputs/hacc/analysis_pipeline_${SUFFIX}.json"
 POWER_SPECTRUM="${HACC}/trunk/Darwin/mpi/bin/hacc_pk_gio_auto"
-PARTICLES_DATA="/projects/exasky_data/hoby/analysis/data-combined-zip-${SUFFIX}-0.90"
-OUTPUT_DATA="/projects/exasky_data/hoby/analysis/pk-combined-zip-${SUFFIX}-0.90.dat"
+PARTICLES_DATA="/projects/exasky_data/hoby/analysis/data-combined-zip-${SUFFIX}-0.95"
+OUTPUT_DATA="/projects/exasky_data/hoby/analysis/pk-combined-zip-${SUFFIX}-0.95.dat"
 STATUS=0
 
 # extract non-halos and compute entropy if required
 if ${EXTRACT_NON_HALOS}; then
   source "/home/hoby/.bashrc" && cd ${BUILD} &&
-  mpirun -np ${NRANKS} ./analyzer ${INPUT_JSON} &&
-  STATUS=$?
+  mpirun -np ${NRANKS} ./analyzer ${INPUT_JSON} 
 fi
 
 # compress non-halo particles dataset
-if ${COMPRESS_NON_HALOS} && [ ${STATUS} -eq 0 ]; then
+if ${COMPRESS_NON_HALOS}; then
   source "/home/hoby/.bashrc" && cd ${BUILD} &&
-  mpirun -np ${NRANKS} ./CBench ${INPUT_JSON} &&
-  STATUS=$?
+  mpirun -np ${NRANKS} ./CBench ${INPUT_JSON} 
 fi
 
 # merge it with halo ones
-if ${MERGE_DATASETS} && [ ${STATUS} -eq 0 ]; then
+if ${MERGE_DATASETS}; then
   source "/home/hoby/.bashrc" && cd ${BUILD} &&
-  mpirun -np ${NRANKS} ./merger ${INPUT_JSON} &&
-  STATUS=$?
+  mpirun -np ${NRANKS} ./merger ${INPUT_JSON}
 fi
 
 # compute power spectrum eventually
-if ${COMPUTE_POWER_SPECTRUM} && [ ${STATUS} -eq 0 ]; then
+if ${COMPUTE_POWER_SPECTRUM}; then
   source "${HACC}.darwin_setup" && cd "${HACC}/run" &&
   mpirun ${POWER_SPECTRUM} inputs/indat.params -n ${PARTICLES_DATA} ${OUTPUT_DATA} ${TIMESTEP}
 fi
