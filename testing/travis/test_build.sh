c48d0acb7dd74965cbcd2b819704bd4f5054e398
Pascal Grosset <pascalgrosset@lanl.gov>
2019-10-02 12:47:47 -0600

    fixed workflow hopefully

--- testing/travis/test_build.sh
+++ testing/travis/test_build.sh
@@ -33,7 +33,7 @@ pip3 install -r testing/travis/requirements.txt
 
 # test HACC workflow generator and executables
 cd Analysis
-python3 -m pat.nyx.workflow --input-file ../inputs/scripts/NYX_workflow.json --preview
+python3 -m pat.nyx.workflow --input-file ../testing/scripts/NYX_workflow.json --preview
 
 #python3 -m pat.hacc.pat_hacc --input-file ../inputs/hacc/hacc_wflow.json --preview
 #python3 -m pat.hacc.pat_hacc_query.py --help
