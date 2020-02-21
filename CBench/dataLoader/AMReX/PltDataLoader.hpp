69fa79f4a3a3a5adeed70099ced513437a8228aa
Pascal Grosset <pascal.av.grosset@gmail.com>
2020-02-21 11:46:24 -0700

    reader for plt images in

--- CBench/dataLoader/AMReX/PltDataLoader.hpp
+++ CBench/dataLoader/AMReX/PltDataLoader.hpp
@@ -36,7 +36,7 @@ class PltDataLoader: public DataLoaderInterface
 
 	void *tempData;
 
-	amrex::ParmParse pp;
+	amrex::DataServices dataServices;
 
   public:
 	PltDataLoader();
@@ -56,7 +56,7 @@ class PltDataLoader: public DataLoaderInterface
 
 inline PltDataLoader::PltDataLoader()
 {
-
+	amrex::Initialize(MPI_COMM_WORLD);
 }
 
 
@@ -75,7 +75,24 @@ inline void PltDataLoader::init(std::string _filename, MPI_Comm _comm)
 	MPI_Comm_size(comm, &numRanks);
 	MPI_Comm_rank(comm, &myRank);
 
-	pp.get("input_path", filename);
+
+	amrex::DataServices::SetBatchMode();
+	amrex::Amrvis::FileType fileType(amrex::Amrvis::NEWPLT);
+	dataServices.Init(filename.c_str(), fileType);
+	if (!dataServices.AmrDataOk()) 
+	{
+		std::cout << "!dataServices.AmrDataOk()" << std::endl;
+		amrex::DataServices::Dispatch(amrex::DataServices::ExitRequest, NULL);
+	}
+
+	if (amrex::ParallelDescriptor::IOProcessor()) 
+	{
+		std::cout << "done." << std::endl;
+	}
+
+	
+	dataType = "double";
+
 	
 
 	log << "original dims: " << origDims[0] << ", " << origDims[1] << ", " << origDims[1] << std::endl;
@@ -87,7 +104,198 @@ inline void PltDataLoader::init(std::string _filename, MPI_Comm _comm)
 
 inline int PltDataLoader::loadData(std::string paramName)
 {
+	//
+	// Read metadata.
+	//
 
+	amrex::AmrData& amrData = dataServices.AmrDataRef();
+	int finestLevel = amrData.FinestLevel();
+
+
+	int nComp = 1;
+	amrex::Vector<int> comps(nComp);
+	int i_dm_scalar(amrData.StateNumber(paramName.c_str()));
+	comps[0] = i_dm_scalar;
+   
+
+	const amrex::Vector<string>& plotVarNames = amrData.PlotVarNames();
+	amrex::Vector<string> inVarNames(nComp);
+	amrex::Vector<int> destFillComps(nComp);
+
+	amrex::ParallelDescriptor::IOProcessor(); 
+  
+	for (int i = 0; i < nComp; ++i) 
+	{
+		inVarNames[i] = plotVarNames[comps[i]];
+		amrex::ParallelDescriptor::IOProcessor();
+		// if (amrex::ParallelDescriptor::IOProcessor()) 
+		// {
+		//     std::cout << "    " << amrData.StateNumber(inVarNames[i])
+		//               << " (" << inVarNames[i] << ")" << std::endl;
+		// }
+		destFillComps[i] = i;
+	}
+
+
+	//
+	// Make boxes and boxarray.
+	//
+
+	// Grab the number of cells in each direction (should be the same, but
+	// we're keeping things general).
+	amrex::Box pd(amrData.ProbDomain()[0]);
+	int64_t grid_nx = pd.bigEnd(0) - pd.smallEnd(0) + 1;
+	int64_t grid_ny = pd.bigEnd(1) - pd.smallEnd(1) + 1;
+	int64_t grid_nz = pd.bigEnd(2) - pd.smallEnd(2) + 1;
+	int64_t num_cells = grid_nx * grid_ny * grid_nz;
+
+	amrex::ParallelDescriptor::IOProcessor();
+
+	// if (amrex::ParallelDescriptor::IOProcessor()) 
+	// {
+	//     printf("The grid has a shape of (%i, %i, %i), %.3e cells total.\n\n",
+	//            (int)grid_nx, (int)grid_ny, (int)grid_nz, (double)num_cells);
+	//     printf("Making skewer chunks.\n");
+	//     fflush(stdout);
+	// }
+
+
+	 // Check if we can split along x evenly.
+	if (grid_nx % myRank != 0) 
+	{
+		if (amrex::ParallelDescriptor::IOProcessor()) 
+		{
+			std::cout << "ERROR: domain decomposition." << std::endl;
+			std::cout << "The number of MPI ranks must fit evenly in the number of cells along x." << std::endl;
+		}
+		MPI_Barrier(comm);
+		amrex::Finalize();
+	}
+
+
+
+	int chunk_size = grid_nx / myRank;
+
+	// The box for z-pencils
+	amrex::Box bx_pencil(amrData.ProbDomain()[0]);
+
+	// List of pencil boxes.
+	amrex::BoxList box_list;
+
+	// indexes
+	int i, ix_lo, ix_hi;
+	for (i=0; i<myRank; ++i) 
+	{
+		ix_lo = chunk_size * i;
+		ix_hi = ix_lo + chunk_size - 1;
+
+		amrex::Box skewer;
+		skewer.setSmall(0, ix_lo);
+		skewer.setBig(0, ix_hi);
+		skewer.setSmall(1, 0);
+		skewer.setBig(1, grid_ny - 1);
+		skewer.setSmall(2, 0);
+		skewer.setBig(2, grid_nz - 1);
+
+		box_list.push_back(skewer);
+	}
+
+	amrex::BoxArray ba(box_list);
+
+	amrex::ParallelDescriptor::IOProcessor(); // Creating multifabs
+
+   
+
+	int num_comps = 1;  // keeping it simple...
+	int comp_start = 0;
+	int level = 0;
+	int ng = 0;         // don't add any ghost zones.
+
+	amrex::DistributionMapping dmap(ba, amrex::ParallelDescriptor::NProcs());
+
+	
+
+
+	//
+	// Start outputting.
+	//
+
+	amrex::MultiFab mf1(ba, dmap, num_comps, ng);
+
+	amrData.FillVar(mf1, level, paramName.c_str(), comp_start);
+	amrData.FlushGrids(amrData.StateNumber(paramName.c_str()));
+
+	if (amrex::ParallelDescriptor::IOProcessor()) 
+	{
+		for (amrex::MFIter mfi(mf1); mfi.isValid(); ++mfi) 
+		{
+			// Get fab, lo and hi vectors.
+			const amrex::Box& box = mfi.validbox();
+			const int *lo_vec = box.loVect();
+			const int *hi_vec = box.hiVect();
+
+			// this is in fortran order.
+			double *fab_data = mf1[mfi].dataPtr();
+
+			// We must make a copy of the fab data in c-order
+			int nx = hi_vec[0] - lo_vec[0] + 1;
+			int ny = hi_vec[1] - lo_vec[1] + 1;
+			int nz = hi_vec[2] - lo_vec[2] + 1;
+			size_t num_cells = nx * ny * nz;
+
+
+			// Passing information to constructor
+			totalNumberOfElements = num_cells;
+
+			dataType = "double";
+			elemSize = 8;
+
+			origDims[0] = nx;
+			origDims[1] = ny;
+			origDims[2] = nz;
+
+
+			// MPI split!!!
+			Partition current = getPartition(myRank, numRanks, origDims[0], origDims[1], origDims[2]);
+			rankOffset[0] = current.min_x;
+			rankOffset[1] = current.min_y;
+			rankOffset[2] = current.min_z;
+
+			sizePerDim[0] = current.max_x - current.min_x;
+			sizePerDim[1] = current.max_y - current.min_y;
+			sizePerDim[2] = current.max_z - current.min_z;
+
+			numElements = sizePerDim[0]*sizePerDim[1]*sizePerDim[2];
+
+
+			// Create space for data and store data there
+			allocateMem(dataType, numElements, 0, data);
+
+
+			size_t indexLocal = 0;
+			for (size_t z=0; z<sizePerDim[2]; z++)
+				for (size_t y=0; y<sizePerDim[1]; y++)
+					for (size_t x=0; x<sizePerDim[0]; x++)
+					{
+						size_t indexGlobal = (rankOffset[2] + z) * (origDims[0] * origDims[1]) +
+											 (rankOffset[1] + y) *  origDims[0] +
+											 (rankOffset[0] + x);
+
+						// Choose based on datatype
+						if (dataType == "float")
+							((float *)data)[indexLocal] = fab_data[indexGlobal];
+						else if (dataType == "double")
+							((double *)data)[indexLocal] = fab_data[indexGlobal];
+						else if (dataType == "int")
+							((int *)data)[indexLocal] = fab_data[indexGlobal];
+						indexLocal++;
+					}
+		}
+	}
+	else
+	{
+		std::cout << "Something failed" << std::endl;
+	}
 }
 
 
--- CBench/test.cpp
+++ CBench/test.cpp
@@ -9,33 +9,41 @@
 
 int main(int argc, char **argv)
 {
-	// if (argc < 3) {
- //        print_usage(argc, argv);
- //    }
+	int rank, nprocs;
+    MPI_Init(&argc,&argv); 
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocs); 
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank); 
 
-	amrex::Initialize(argc, argv);
 
-    //amrex::ParmParse::Initialize(argc-2, argv+2, "/bigData/plt02669");
+    amrex::Initialize(MPI_COMM_WORLD);
 
 
     amrex::DataServices::SetBatchMode();
     amrex::Amrvis::FileType fileType(amrex::Amrvis::NEWPLT);
     amrex::DataServices dataServices( "/bigData/plt02669", fileType);
+    if (!dataServices.AmrDataOk()) 
+    {
+    	std::cout << "!dataServices.AmrDataOk()" << std::endl;
+        amrex::DataServices::Dispatch(amrex::DataServices::ExitRequest, NULL);
+    }
+
 
     if (amrex::ParallelDescriptor::IOProcessor()) 
     {
-        std::cout << " done." << std::endl;
+        std::cout << "done." << std::endl;
     }
-    else
-    {
-    	std::cout << "Failed!" << std::endl;
-    }
+   
 
 
     amrex::AmrData& amrData = dataServices.AmrDataRef();
     int finestLevel = amrData.FinestLevel();
+
     int Nlev = finestLevel + 1;
 
+
+    //
+    // Read metadata.
+    //
 
     int nComp = 2;
     amrex::Vector<int> comps(nComp);
@@ -65,13 +73,61 @@ int main(int argc, char **argv)
         destFillComps[i] = i;
     }
 
-    int num_comps = 1;  // keeping it simple...
-    int comp_start = 0;
-    int level = 0;
-    int ng = 0;         // don't add any ghost zones.
 
+    //
+    // Make boxes and boxarray.
+    //
+
+    // Grab the number of cells in each direction (should be the same, but
+    // we're keeping things general).
+    amrex::Box pd(amrData.ProbDomain()[0]);
+    int64_t grid_nx = pd.bigEnd(0) - pd.smallEnd(0) + 1;
+    int64_t grid_ny = pd.bigEnd(1) - pd.smallEnd(1) + 1;
+    int64_t grid_nz = pd.bigEnd(2) - pd.smallEnd(2) + 1;
+    int64_t num_cells = grid_nx * grid_ny * grid_nz;
+
+    if (amrex::ParallelDescriptor::IOProcessor()) {
+        printf("The grid has a shape of (%i, %i, %i), %.3e cells total.\n\n",
+               (int)grid_nx, (int)grid_ny, (int)grid_nz, (double)num_cells);
+        printf("Making skewer chunks.\n");
+        fflush(stdout);
+    }
+
+
+     // Check if we can split along x evenly.
+    if (grid_nx % nprocs != 0) {
+        if (amrex::ParallelDescriptor::IOProcessor()) {
+            printf("ERROR: domain decomposition.\n");
+            printf("The number of MPI ranks must fit evenly in the number of cells along x.\n");
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        amrex::Finalize();
+    }
+
+
+
+    int chunk_size = grid_nx / nprocs;
+
+    // The box for z-pencils
+    amrex::Box bx_pencil(amrData.ProbDomain()[0]);
+    // List of pencil boxes.
     amrex::BoxList box_list;
+    // indexes
+    int i, ix_lo, ix_hi;
+    for (i = 0; i < nprocs; ++i) {
+        ix_lo = chunk_size * i;
+        ix_hi = ix_lo + chunk_size - 1;
 
+        amrex::Box skewer;
+        skewer.setSmall(0, ix_lo);
+        skewer.setBig(0, ix_hi);
+        skewer.setSmall(1, 0);
+        skewer.setBig(1, grid_ny - 1);
+        skewer.setSmall(2, 0);
+        skewer.setBig(2, grid_nz - 1);
+
+        box_list.push_back(skewer);
+    }
 
     amrex::BoxArray ba(box_list);
 
@@ -79,32 +135,137 @@ int main(int argc, char **argv)
         std::cout << "Creating multifabs." << std::endl;
     }
 
+
+    int num_comps = 1;  // keeping it simple...
+    int comp_start = 0;
+    int level = 0;
+    int ng = 0;         // don't add any ghost zones.
+
+
     amrex::DistributionMapping dmap(ba, amrex::ParallelDescriptor::NProcs());
 
+    
+
+
+    //
+    // Start outputting.
+    //
+
+    // if (amrex::ParallelDescriptor::IOProcessor()) {
+    //     std::cout << "Preparing output file." << std::endl;
+    // }
+
+
+    //
+    // temperature
+    //
+
     amrex::MultiFab mf1(ba, dmap, num_comps, ng);
+
 
     amrData.FillVar(mf1, level, "Temp", comp_start);
     amrData.FlushGrids(amrData.StateNumber("Temp"));
 
-    std::cout << "valid " << amrData.StateNumber("Temp") << std::endl;
-
-    amrex::MFIter mfi(mf1);
-    std::cout << "valid " << mfi.isValid() << std::endl;
-
-    for (amrex::MFIter mfi(mf1); mfi.isValid(); ++mfi) 
+    if (amrex::ParallelDescriptor::IOProcessor()) 
     {
-        // this is in fortran order.
-        double *fab_data = mf1[mfi].dataPtr();
-        std::cout << fab_data[0] << std::endl;
-        std::cout << fab_data[1] << std::endl;
-        std::cout << fab_data[2] << std::endl;
-        std::cout << fab_data[3] << std::endl;
-        std::cout << fab_data[4] << std::endl;
-    }
+	    std::cout << "valid Temp: " << amrData.StateNumber("Temp") << std::endl;
 
-    // std::string infile; 
-    // std::cout << "Reading " << infile << "...";
-    // pp.get("infile",infile);
+	    amrex::MFIter mfi(mf1);
+	    std::cout << "mfi.isValid(): " << mfi.isValid() << std::endl;
+
+
+	    for (amrex::MFIter mfi(mf1); mfi.isValid(); ++mfi) 
+	    {
+	    	// Get fab, lo and hi vectors.
+	        const amrex::Box& box = mfi.validbox();
+	        const int *lo_vec = box.loVect();
+	        const int *hi_vec = box.hiVect();
+
+	        // this is in fortran order.
+	        double *fab_data = mf1[mfi].dataPtr();
+
+	        // We must make a copy of the fab data in c-order
+	        int nx = hi_vec[0] - lo_vec[0] + 1;
+	        int ny = hi_vec[1] - lo_vec[1] + 1;
+	        int nz = hi_vec[2] - lo_vec[2] + 1;
+	        size_t num_cells = nx * ny * nz;
+
+	        // this is in fortran order.
+	        std::cout << fab_data[0] << std::endl;
+	        std::cout << fab_data[1] << std::endl;
+	        std::cout << fab_data[2] << std::endl;
+	        std::cout << fab_data[3] << std::endl;
+	        std::cout << fab_data[4] << std::endl;
+	    }
+
+	    // std::string infile; 
+	    // std::cout << "Reading " << infile << "...";
+	    // pp.get("infile",infile);
+	}
+	else
+	{
+		std::cout << "Something failed" << std::endl;
+	}
+
+
+
+	//
+    // density
+    //
+
+    amrex::MultiFab mf2(ba, dmap, num_comps, ng);
+
+	amrData.FillVar(mf2, level, "density", comp_start);
+    amrData.FlushGrids(amrData.StateNumber("density"));
+
+    if (amrex::ParallelDescriptor::IOProcessor()) 
+    {
+	    std::cout << "valid Temp: " << amrData.StateNumber("Temp") << std::endl;
+
+	    amrex::MFIter mfi(mf2);
+	    std::cout << "mfi.isValid(): " << mfi.isValid() << std::endl;
+
+
+	    for (amrex::MFIter mfi(mf2); mfi.isValid(); ++mfi) 
+	    {
+	    	// Get fab, lo and hi vectors.
+	        const amrex::Box& box = mfi.validbox();
+	        const int *lo_vec = box.loVect();
+	        const int *hi_vec = box.hiVect();
+
+	        // this is in fortran order.
+	        double *fab_data = mf2[mfi].dataPtr();
+
+	        // We must make a copy of the fab data in c-order
+	        int nx = hi_vec[0] - lo_vec[0] + 1;
+	        int ny = hi_vec[1] - lo_vec[1] + 1;
+	        int nz = hi_vec[2] - lo_vec[2] + 1;
+	        size_t num_cells = nx * ny * nz;
+
+	        std::cout << "nx: " << nx << std::endl;
+	        std::cout << "ny: " << ny << std::endl;
+	        std::cout << "nz: " << nz << std::endl;
+
+	        // this is in fortran order.
+	        std::cout << fab_data[0] << std::endl;
+	        std::cout << fab_data[1] << std::endl;
+	        std::cout << fab_data[2] << std::endl;
+	        std::cout << fab_data[3] << std::endl;
+	        std::cout << fab_data[4] << std::endl;
+	    }
+
+	    // std::string infile; 
+	    // std::cout << "Reading " << infile << "...";
+	    // pp.get("infile",infile);
+	}
+	else
+	{
+		std::cout << "Something failed" << std::endl;
+	}
+
+	amrex::ParallelDescriptor::Barrier();
+    amrex::Finalize();
+    MPI_Finalize(); 
 	
 	return 0;
 }
