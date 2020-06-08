d116f8beea1cdfc62e878cd2aa4298efa230210b
jinsian <sjin6@crimson.ua.edu>
2020-06-08 12:18:19 -0700

    test update

--- CBench/compressors/SZ/SZcompressor.hpp
+++ CBench/compressors/SZ/SZcompressor.hpp
@@ -81,6 +81,37 @@ inline int SZCompressor::compress(void *input, void *&output, std::string dataTy
 			// Unknown mode, just fill in input to SZ
 		}
 
+
+        float entropy = 0;
+        float value_max = 0;
+        float value_min = 3.402823466e+38F;
+        int pos[128];
+        for (int i = 0; i < 128; i++)
+                pos[i] = 0;
+        for (int i = 0; i < numel; i++) {
+                if (static_cast<float *>(input)[i] > value_max)
+                        value_max = static_cast<float *>(input)[i];
+                if (static_cast<float *>(input)[i] < value_min)
+                        value_min = static_cast<float *>(input)[i];
+        }
+        float minmax = value_max - value_min;
+        printf("min = %f, max = %f, minmax = %f, numel = %d\n", value_min, value_max, minmax, numel);
+        for (int i = 0; i < numel; i++)
+                pos[(int)floor(((static_cast<float *>(input)[i] - value_min)/minmax) * 128)] += 1;
+        for (int i = 0; i < 128; i++) {
+                if (pos[i] != 0)
+                        entropy += (-float(pos[i])/numel) * (std::log(float(pos[i])/numel)/std::log(2));
+        }
+        printf("entropy = %f ", entropy);
+        //free(pos);
+//      absTol = (1/entropy)/800;
+        absTol = entropy * 10;
+        printf("error bound = %f\n", absTol);
+
+
+//      printf("data start = %f %f %f %f\n", static_cast<float *>(input)[0], static_cast<float *>(input)[1], static_cast<float *>(input)[134217726], static_cast<float *>(input)[134217727]);
+
+
 	std::size_t csize = 0;
 	std::uint8_t *cdata = SZ_compress_args(SZ_FLOAT, static_cast<float *>(input), &csize, mode, absTol, relTol, powerTol, n[4], n[3], n[2], n[1], n[0]);
 
